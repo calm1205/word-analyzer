0518f1f6708690ba2ec3e67c9c1dd8d84eead9c4
calm1205 <authentic.nagi@gmail.com>
2024-10-07 17:37:29 +0900

    feat: add fixtures and get comment

---
+++ src/get_comments.py
@@ -0,0 +1,16 @@
+import aspose.words as aw
+
+doc = aw.Document('fixtures/' + 'original_comment.docx')
+comments = doc.get_child_nodes(aw.NodeType.COMMENT, True)
+# If a comment has no ancestor, it is a "top-level" comment as opposed to a reply-type comment.
+# Print all top-level comments along with any replies they may have.
+for comment in comments:
+    comment = comment.as_comment()
+    if comment.ancestor is None:
+        print('Top-level comment:')
+        print(f'\t"{comment.get_text().strip()}", by {comment.author}')
+        print(f'Has {comment.replies.count} replies')
+        for comment_reply in comment.replies:
+            comment_reply = comment_reply.as_comment()
+            print(f'\t"{comment_reply.get_text().strip()}", by {comment_reply.author}')
+        print()
--- src/import_path.py
+++
@@ -1,5 +0,0 @@
-import sys
-print(sys.path)
-
-import ssl
-print(ssl.OPENSSL_VERSION)
