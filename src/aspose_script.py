4e88f654453588d99af167bd27f8925897fc7bb2
calm1205 <authentic.nagi@gmail.com>
2024-10-07 16:50:51 +0900

    initial commit

---
+++ src/aspose_script.py
@@ -0,0 +1,12 @@
+import aspose.words as aw
+
+# Create a blank document.
+doc = aw.Document()
+
+# Use a document builder to add content to the document.
+builder = aw.DocumentBuilder(doc)
+# Write a new paragraph in the document with the text "Hello World!".
+builder.writeln("Hello, World!")
+
+# Save the document in DOCX format. Save format is automatically determined from the file extension.
+doc.save("output.docx")
---
+++ src/import_path.py
@@ -0,0 +1,5 @@
+import sys
+print(sys.path)
+
+import ssl
+print(ssl.OPENSSL_VERSION)
