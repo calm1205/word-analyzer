7a74b3568481c80621d950d2ca7ba618e959b29b
calm1205 <authentic.nagi@gmail.com>
2024-10-08 16:58:38 +0900

    feat(spire): read custom properties

---
+++ src/spire/read_custom_properties.py
@@ -0,0 +1,14 @@
+from spire.doc import *
+from spire.doc.common import *
+from pprint import pprint
+
+# Create a Document object
+doc = Document()
+
+# Load a Word file
+doc.LoadFromFile("fixtures/original_custom_properties.docx")
+
+# Accept all revisions
+for i in range(doc.CustomDocumentProperties.Count):
+    pprint(doc.CustomDocumentProperties[i].Name)
+    pprint(vars(doc.CustomDocumentProperties[i].Value)) # 具体的なオブジェクトが取れない
