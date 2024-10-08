3b09d759ef670b652ad1b39c5d0d8193ad938332
calm1205 <authentic.nagi@gmail.com>
2024-10-08 11:50:10 +0900

    feat: add english docx

--- src/aspose/convert_pdf.py
+++ src/aspose/convert_pdf.py
@@ -1,5 +1,6 @@
 import aspose.words as aw
 
 doc = aw.Document("fixtures/original.docx")
+# doc = aw.Document("fixtures/original.eng.docx") # english
 
 doc.save("output/original.pdf", aw.SaveFormat.PDF)
