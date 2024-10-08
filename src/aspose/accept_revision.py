bbef98954df2b672082f6af74f0dedf583307fb6
calm1205 <authentic.nagi@gmail.com>
2024-10-08 11:43:17 +0900

    fix(aspose): path

--- src/aspose/accept_revision.py
+++ src/aspose/accept_revision.py
@@ -1,5 +1,5 @@
 import aspose.words as aw
 
-doc = aw.Document('fixtures/' + 'original_revision.docx')
+doc = aw.Document('fixtures/original_revision.docx')
 doc.revisions.accept_all()
 doc.save('output.docx')
