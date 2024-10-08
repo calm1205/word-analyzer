83e87ccfa6c9ddbe276cb1da6cba865ce5c155c9
calm1205 <authentic.nagi@gmail.com>
2024-10-08 12:57:17 +0900

    fix: convert pdf

--- src/aspose/convert_pdf.py
+++ src/aspose/convert_pdf.py
@@ -1,6 +1,12 @@
 import aspose.words as aw
 
-doc = aw.Document("fixtures/original.docx")
-# doc = aw.Document("fixtures/original.eng.docx") # english
+# for English
+# doc = aw.Document("fixtures/original.eng.docx")
+# doc.save("output/original.pdf", aw.SaveFormat.PDF)
 
-doc.save("output/original.pdf", aw.SaveFormat.PDF)
+# for Japanese (not working) 文字化け回避策が分からず
+doc = aw.Document("fixtures/original.docx")
+save_options = aw.saving.PdfSaveOptions()
+save_options.embed_full_fonts = True
+
+doc.save("output/original.pdf", save_options)
