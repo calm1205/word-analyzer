45456b02a8e7551ee2e064cf9effd10aedc997d9
calm1205 <authentic.nagi@gmail.com>
2024-10-08 11:34:56 +0900

    feat(aspose): set custom properties

--- src/aspose/set_custom_properties.py
+++ src/aspose/set_custom_properties.py
@@ -2,6 +2,8 @@ import aspose.words as aw
 
 doc = aw.Document("fixtures/original.docx")
 
-doc.custom_document_properties.add("MNTSQ", True)
+doc.custom_document_properties.add("MNTSQ_boolean", True)
+doc.custom_document_properties.add("MNTSQ_number", 123)
+doc.custom_document_properties.add("MNTSQ_string", "Hello, World!")
 
 doc.save("output/original_custom_properties.docx")
