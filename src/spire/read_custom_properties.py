d952ade5143745a08826fd65116e0aadd34b49f5
calm1205 <authentic.nagi@gmail.com>
2024-10-09 15:57:52 +0900

    feat(spire): fixtures

--- src/spire/read_custom_properties.py
+++ src/spire/read_custom_properties.py
@@ -5,14 +5,11 @@ from pprint import pprint
 # build-in propertiesはドキュメントが存在するが、custom propertiesはドキュメントなし
 # ref: https://www.e-iceblue.com/Tutorials/Python/Spire.Doc-for-Python/Program-Guide/Document-Operation/Python-Add-Read-and-Remove-Built-in-Document-Properties-in-Word-Documents.html
 
-# Create a Document object
 doc = Document()
 
-# Load a Word file
 # doc.LoadFromFile("fixtures/original_custom_properties.doc")
 doc.LoadFromFile("fixtures/original_custom_properties.docx")
 
-# Read custom properties
 for i in range(doc.CustomDocumentProperties.Count):
     name = doc.CustomDocumentProperties[i].Name
     value = doc.CustomDocumentProperties.get_Item(name).ToString()
--- src/spire/update_custom_properties.py
+++ src/spire/update_custom_properties.py
@@ -5,15 +5,11 @@ from pprint import pprint
 # build-in propertiesはドキュメントが存在するが、custom propertiesはドキュメントなし
 # ref: https://www.e-iceblue.com/Tutorials/Python/Spire.Doc-for-Python/Program-Guide/Document-Operation/Python-Add-Read-and-Remove-Built-in-Document-Properties-in-Word-Documents.html
 
-# Create a Document object
 doc = Document()
 
-# Load a Word file
 doc.LoadFromFile("fixtures/original.doc")
 # doc.LoadFromFile("fixtures/original.docx")
 
-# Add custom properties
 doc.CustomDocumentProperties.Add("MNTSQ_boolean", "")
 
-# Save as New File
 doc.SaveToFile("output/original_custom_properties.docx")
